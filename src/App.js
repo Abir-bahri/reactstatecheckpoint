feb463b2e670510c7b2956dbae3449dca3e5f7f3
Abir-bahri <bahriabir15@gmail.com>
2022-04-06 10:36:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import myImage from './profile.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      Person: {
+        fullName: "Bahri Abir",
+        bio: "love developing",
+        imgSrc: myImage,
+        profession: "electronics engineer",
+        date: new Date()
+      },
+      show: true
+    };
+  }
+  
+    handleShowPerson = () => {
+      this.setState({
+        ...this.state,
+        show: !this.state.show
+      });
+    };
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    };
+   
+    
+  
+    componentWillUnmount() {
+      
+    }
+   
+ 
+    render() {
+      return (
+        <div className='App-header'>
+          {this.state.show && (
+            <>
+              <h1>{this.state.Person.fullName}</h1>
+              <h1>{this.state.Person.bio}</h1>
+              <img src={this.state.Person.imgSrc} alt="profile"></img>
+              <h1>{this.state.Person.profession}</h1>
+              
+              <br></br>
+              
+            </>
+           
+          )}
+          <h2>It is {this.state.date}.</h2>
+          <button onClick={this.handleShowPerson}>click me</button>
+        </div>
+      );
+    }
+  }
+  
 
 export default App;
